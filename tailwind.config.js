26a2977989398b3aeecef868a6f3cb111aa31f7a
PeterHCM <phucnk@weidmuller.vn>
2025-10-08 21:41:32 +0700

    HeroCarousel+ImageBlock

--- tailwind.config.js
+++ tailwind.config.js
@@ -116,6 +116,19 @@ module.exports = {
     'delay-[250ms]',
     'delay-[300ms]',
 
+    // alertClass từ switch C#
+    'bg-green-100', 'text-green-800', 'border-green-300',
+    'dark:bg-green-900', 'dark:text-green-100', 'dark:border-green-700',
+
+    'bg-yellow-100', 'text-yellow-800', 'border-yellow-300',
+    'dark:bg-yellow-900', 'dark:text-yellow-100', 'dark:border-yellow-700',
+
+    'bg-red-100', 'text-red-800', 'border-red-300',
+    'dark:bg-red-900', 'dark:text-red-100', 'dark:border-red-700',
+
+    'bg-blue-100', 'text-blue-800', 'border-blue-300',
+    'dark:bg-blue-900', 'dark:text-blue-100', 'dark:border-blue-700',
+
     // col-span
     ...Array.from({ length: 12 }, (_, i) => `col-span-${i + 1}`),
     ...Array.from({ length: 12 }, (_, i) => `sm:col-span-${i + 1}`),
