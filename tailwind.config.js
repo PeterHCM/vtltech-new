ec59aa1a8117904ce617b138ed07d88c2e19cf21
PeterHCM <phucnk@weidmuller.vn>
2025-10-19 22:48:39 +0700

    HeroCarousel+ImageBlock

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,12 @@ module.exports = {
     './wwwroot/Views/**/*.cshtml',
     './wwwroot/js/**/*.js',
   ],
-  darkMode: 'class', // Sử dụng class 'dark' để bật/tắt chế độ tối
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"Roboto"', '"Roboto Condensed"', 'ui-sans-serif', 'system-ui', 'sans-serif'],
-        condensed: ['"Roboto Condensed"', 'ui-sans-serif', 'system-ui', 'sans-serif'],
-        mono: ['"Roboto Mono"', 'ui-monospace', 'SFMono-Regular', 'Menlo', 'monospace'],
+        sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+        mono: ['ui-monospace', 'SFMono-Regular', 'Menlo', 'monospace'],
       },
       colors: {
         primary: '#e06800',
@@ -51,64 +50,64 @@ module.exports = {
       typography: (theme) => ({
         DEFAULT: {
           css: {
+            fontFamily: theme('fontFamily.sans').join(', '),
             color: theme('colors.gray.800'),
             'h1, h2, h3, h4, h5, h6': {
               color: theme('colors.primary'),
+              fontFamily: theme('fontFamily.sans').join(', '),
             },
-            'a': {
+            a: {
               color: theme('colors.gray.800'),
               textDecoration: 'none',
               '&:hover': {
                 color: theme('colors.primary'),
               },
             },
-            'blockquote': {
+            blockquote: {
               borderLeftColor: theme('colors.gray.300'),
               color: theme('colors.gray.500'),
             },
-            'ul, ol': {
-              'list-style-type': 'disc',
-              'padding-left': '1.5rem',
+            ul: { listStyleType: 'disc', paddingLeft: '1.5rem' },
+            ol: { listStyleType: 'disc', paddingLeft: '1.5rem' },
+            li: {
+              color: theme('colors.gray.800'),
             },
-            'li': {
+            'li::marker': {
               color: theme('colors.gray.800'),
             },
             'code::before': { content: '""' },
             'code::after': { content: '""' },
-            'li::marker': {
-                color: theme('colors.gray.800'),
-            },
           },
         },
         dark: {
           css: {
+            fontFamily: theme('fontFamily.sans').join(', '),
             color: theme('colors.gray.200'),
             'h1, h2, h3, h4, h5, h6': {
               color: theme('colors.white'),
+              fontFamily: theme('fontFamily.sans').join(', '),
             },
-            'strong': {
+            strong: {
               color: theme('colors.white'),
             },
-            'a': {
+            a: {
               color: theme('colors.gray.200'),
               textDecoration: 'none',
               '&:hover': {
                 color: theme('colors.primary'),
               },
             },
-            'blockquote': {
+            blockquote: {
               borderLeftColor: theme('colors.gray.700'),
               color: theme('colors.gray.400'),
             },
-            'ul, ol': {
-              'list-style-type': 'disc',
-              'padding-left': '1.5rem',
-            },
-            'li': {
+            ul: { listStyleType: 'disc', paddingLeft: '1.5rem' },
+            ol: { listStyleType: 'disc', paddingLeft: '1.5rem' },
+            li: {
               color: theme('colors.gray.200'),
             },
             'li::marker': {
-                color: theme('colors.gray.200'),
+              color: theme('colors.gray.200'),
             },
           },
         },
@@ -116,46 +115,28 @@ module.exports = {
     },
   },
   safelist: [
-    // Hiệu ứng delay
     'peer-checked:rotate-90',
-    'delay-[0ms]',
-    'delay-[50ms]',
-    'delay-[100ms]',
-    'delay-[150ms]',
-    'delay-[200ms]',
-    'delay-[250ms]',
-    'delay-[300ms]',
-
-    // alertClass từ switch C#
+    'delay-[0ms]', 'delay-[50ms]', 'delay-[100ms]', 'delay-[150ms]', 'delay-[200ms]', 'delay-[250ms]', 'delay-[300ms]',
     'bg-green-100', 'text-green-800', 'border-green-300',
     'dark:bg-green-900', 'dark:text-green-100', 'dark:border-green-700',
-
     'bg-yellow-100', 'text-yellow-800', 'border-yellow-300',
     'dark:bg-yellow-900', 'dark:text-yellow-100', 'dark:border-yellow-700',
-
     'bg-red-100', 'text-red-800', 'border-red-300',
     'dark:bg-red-900', 'dark:text-red-100', 'dark:border-red-700',
-
     'bg-blue-100', 'text-blue-800', 'border-blue-300',
     'dark:bg-blue-900', 'dark:text-blue-100', 'dark:border-blue-700',
-
-    // col-span
     ...Array.from({ length: 12 }, (_, i) => `col-span-${i + 1}`),
     ...Array.from({ length: 12 }, (_, i) => `sm:col-span-${i + 1}`),
     ...Array.from({ length: 12 }, (_, i) => `md:col-span-${i + 1}`),
     ...Array.from({ length: 12 }, (_, i) => `lg:col-span-${i + 1}`),
     ...Array.from({ length: 12 }, (_, i) => `xl:col-span-${i + 1}`),
     ...Array.from({ length: 12 }, (_, i) => `2xl:col-span-${i + 1}`),
-
-    // grid-cols
     ...Array.from({ length: 12 }, (_, i) => `grid-cols-${i + 1}`),
     ...Array.from({ length: 12 }, (_, i) => `sm:grid-cols-${i + 1}`),
     ...Array.from({ length: 12 }, (_, i) => `md:grid-cols-${i + 1}`),
     ...Array.from({ length: 12 }, (_, i) => `lg:grid-cols-${i + 1}`),
     ...Array.from({ length: 12 }, (_, i) => `xl:grid-cols-${i + 1}`),
     ...Array.from({ length: 12 }, (_, i) => `2xl:grid-cols-${i + 1}`),
-
-    // row-span
     ...Array.from({ length: 12 }, (_, i) => `row-span-${i + 1}`),
     ...Array.from({ length: 12 }, (_, i) => `sm:row-span-${i + 1}`),
     ...Array.from({ length: 12 }, (_, i) => `md:row-span-${i + 1}`),
